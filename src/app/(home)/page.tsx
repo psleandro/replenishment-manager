31d7b3301f643cc7315a048b27bc52aa00a3081c
Leandro Santos <lsaantos@outlook.com>
2023-07-13 22:55:04 -0300

    fix: add error handling in home page fetching to avoid build error

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,17 @@
 import { getReplenishments } from '~/services/replenishment';
 
 import { ReplenishmentManager } from './ReplenishmentManager';
+import type { Replenishment } from '~/@types';
 
 export default async function Home() {
-  const replenishments = await getReplenishments();
+  let replenishments: Replenishment[] = [];
+
+  try {
+    const results = await getReplenishments();
+    replenishments = results;
+  } catch (error){
+    console.log('Home error: ', error);
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center p-3 transition-all lg:p-24">
